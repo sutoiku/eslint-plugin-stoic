dc3c113591a007003e4ab0a0d9bf18265aa4f073
Yves Le Maout <yves@sutoiku.com>
2016-08-16 13:34:05 -0400

    Cleanup eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,8 @@ const fs       = require('fs');
 const jshintrc = JSON.parse(fs.readFileSync(`${__dirname}/.jshintrc`, 'utf8'));
 const globals  = jshintrc.predef.reduce((acc, v) => { acc[v] = true; return acc; }, {});
 
-require('./test/kyu-linter');
-
 module.exports = {
   globals,
   extends : './node_modules/eslint-config-bestpractices/index.js',
-  env     : { es6: true, node: true, browser: true },
-  plugins : [ 'kyu-linter' ],
-  rules   : {
-    'kyu-linter/no-undefined-fermat-functions' : ['error'],
-    'kyu-linter/no-done-without-error-class'   : ['error']
-  }
+  env     : { es6: true, node: true, browser: true }
 };
