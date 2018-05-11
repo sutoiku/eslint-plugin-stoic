35d4a1b8bd4b95edeb75db321f5616bd97819deb
Aurelien Ribon <aurelien.ribon@gmail.com>
2018-05-11 09:26:40 -0700

    Add custom rule `no-only-property`

---
+++ tests/lib/rules/no-only-property.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const rule       = require('../../../lib/rules/no-only-property');
+const RuleTester = require('eslint').RuleTester;
+
+new RuleTester().run('no-only-property', rule, {
+  valid   : [{ code: '({ foo: 1 })' }],
+  invalid : [{
+    code   : '({ only: true })',
+    errors : [{ message: `Property "only" should be removed.`, type: 'Property' }]
+  }]
+});
