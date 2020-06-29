4850a634ef7432fe217dbf390cf7e5062d719bd8
Aurelien Ribon <aurelien.ribon@gmail.com>
2020-06-29 13:58:46 +0200

    Prettify code

--- tests/lib/rules/no-done-without-error-class.js
+++ tests/lib/rules/no-done-without-error-class.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const rule       = require('../../../lib/rules/no-done-without-error-class');
+const rule = require('../../../lib/rules/no-done-without-error-class');
 const RuleTester = require('eslint').RuleTester;
 
 new RuleTester().run('no-done-without-error-class', rule, {
-  valid   : [{ code: 'done(new Error("error"))' },
-             { code: 'done(null, res)' },
-             { code: 'done()' }],
-  invalid : [{
-    code   : 'done("error")',
-    errors : [{ message: `"done" called without error class`, type: 'CallExpression' }]
-  }]
+  valid: [{ code: 'done(new Error("error"))' }, { code: 'done(null, res)' }, { code: 'done()' }],
+  invalid: [
+    {
+      code: 'done("error")',
+      errors: [{ message: `"done" called without error class`, type: 'CallExpression' }],
+    },
+  ],
 });
--- tests/lib/rules/no-only-property.js
+++ tests/lib/rules/no-only-property.js
@@ -1,12 +1,14 @@
 'use strict';
 
-const rule       = require('../../../lib/rules/no-only-property');
+const rule = require('../../../lib/rules/no-only-property');
 const RuleTester = require('eslint').RuleTester;
 
 new RuleTester().run('no-only-property', rule, {
-  valid   : [{ code: '({ foo: 1 })' }],
-  invalid : [{
-    code   : '({ only: true })',
-    errors : [{ message: `Property "only" should be removed.`, type: 'Property' }]
-  }]
+  valid: [{ code: '({ foo: 1 })' }],
+  invalid: [
+    {
+      code: '({ only: true })',
+      errors: [{ message: `Property 'only' should be removed.`, type: 'Property' }],
+    },
+  ],
 });
